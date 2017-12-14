d05eda00df354ba46abb60ecfa4a97e49a6e33f3
BasilJohn <ferdenend@gmail.com>
2017-12-14 15:10:49 +0530

    Trip List Header change name based on screen
    Trip List Header change name based on screen

--- src/components/Identity/Login.js
+++ src/components/Identity/Login.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import { View, Text, StyleSheet, KeyboardAvoidingView, ScrollView } from 'react-native';
-import { Button, Input, Spinner, Block, BlockDetail, NavigationLink } from '../common';
-import { onEmailChanged, onPasswordChanged,onLoginUser,setNavigationProps } from '../../actions';
-import MainFeed from '../Initial/MainFeed';
-import firebase from 'firebase';
-import { connect } from 'react-redux';
-
-
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  KeyboardAvoidingView,
+  ScrollView
+} from "react-native";
+import {
+  Button,
+  Input,
+  Spinner,
+  Block,
+  BlockDetail,
+  NavigationLink
+} from "../common";
+import {
+  onEmailChanged,
+  onPasswordChanged,
+  onLoginUser,
+  setNavigationProps
+} from "../../actions";
+import MainFeed from "../Initial/MainFeed";
+import firebase from "firebase";
+import { connect } from "react-redux";
 
 class Login extends React.Component {
-
   state = { loggedIn: false };
-
 
   static navigationOptions = ({ navigation }) => {
     const { params = {} } = navigation.state;
     return {
-      title: 'Login',
-      headerTintColor: '#F1F1F2',
-      headerTitleStyle: { color: '#F1F1F2' },
-      headerRight: <NavigationLink linkText={params.showAddTrip == '1' ? 'Add Trip' : ''} onPress={() => navigation.navigate('AddTrip')} />
-
+      title: params.showAddTrip == "1" ? "Trip List" : "Login",
+      headerTintColor: "#F1F1F2",
+      headerTitleStyle: { color: "#F1F1F2" },
+      headerRight: (
+        <NavigationLink
+          linkText={params.showAddTrip == "1" ? "Add Trip" : ""}
+          onPress={() => navigation.navigate("AddTrip")}
+        />
+      )
     };
   };
 
@@ -32,19 +50,18 @@ class Login extends React.Component {
   }
 
   componentDidMount() {
-    firebase.auth().onAuthStateChanged((user) => {
+    firebase.auth().onAuthStateChanged(user => {
       if (user) {
         this.setState({ loggedIn: true });
         this.props.navigation.setParams({
-          showAddTrip: '1',
+          showAddTrip: "1"
         });
       } else {
         this.setState({ loggedIn: false });
         this.props.navigation.setParams({
-          showAddTrip: '0',
+          showAddTrip: "0"
         });
       }
-
     });
 
     this.props.setNavigationProps(this.props.navigation);
@@ -52,97 +69,99 @@ class Login extends React.Component {
 
   onLoginButtonPress() {
     const { email, password } = this.props;
-     this.props. onLoginUser( { email,password});  
+    this.props.onLoginUser({ email, password });
   }
-  
 
   renderButton() {
     if (this.props.loading) {
       return <Spinner />;
     }
-    return (<Button onPress={this.onLoginButtonPress.bind(this)} buttonText={'Login'} />);
+    return (
+      <Button
+        onPress={this.onLoginButtonPress.bind(this)}
+        buttonText={"Login"}
+      />
+    );
   }
 
   renderContent() {
-
     switch (this.state.loggedIn) {
       case true:
         return <MainFeed />;
       case false:
         return (
           <ScrollView contentContainerStyle={styles.loginStyle}>
-          <KeyboardAvoidingView behavior="position" >
-            <Block>
-              <BlockDetail>
-                <Input
-                  placeholder={'user@gmail.com'}
-                  inputText={'Email'}
-                  value={this.props.email}
-                  onChangeText={this.onEmailChange.bind(this)} />
-                <Input
-                  secureTextEntry
-                  placeholder={'Password'}
-                  inputText={'Password'}
-                  value={this.props.password}
-                  onChangeText={this.onPasswordChange.bind(this)} />
-                <Text style={styles.errorTextStyle}>
-                  {this.props.error}
-                </Text>
-                <View >
-                  {this.renderButton()}
-                </View>
-              </BlockDetail>
-            </Block>
-          </KeyboardAvoidingView>
-          </ScrollView>);
+            <KeyboardAvoidingView behavior="position">
+              <Block>
+                <BlockDetail>
+                  <Input
+                    placeholder={"user@gmail.com"}
+                    inputText={"Email"}
+                    value={this.props.email}
+                    onChangeText={this.onEmailChange.bind(this)}
+                  />
+                  <Input
+                    secureTextEntry
+                    placeholder={"Password"}
+                    inputText={"Password"}
+                    value={this.props.password}
+                    onChangeText={this.onPasswordChange.bind(this)}
+                  />
+                  <Text style={styles.errorTextStyle}>{this.props.error}</Text>
+                  <View>{this.renderButton()}</View>
+                </BlockDetail>
+              </Block>
+            </KeyboardAvoidingView>
+          </ScrollView>
+        );
       default:
-        return (<Spinner />);
+        return <Spinner />;
     }
   }
 
   render(props) {
     return (
-      <View style={styles.contentContainerStyle}>
-        {this.renderContent()}
-      </View>
+      <View style={styles.contentContainerStyle}>{this.renderContent()}</View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-
   errorTextStyle: {
-    color: 'red',
-    alignSelf: 'center',
-    fontSize: 20,
+    color: "red",
+    alignSelf: "center",
+    fontSize: 20
   },
   contentContainerStyle: {
-    flex:1,
-    justifyContent:'center',
-    flexDirection:'column',
-    backgroundColor: '#fff',
+    flex: 1,
+    justifyContent: "center",
+    flexDirection: "column",
+    backgroundColor: "#fff"
   },
-  loginStyle:{
+  loginStyle: {
     flexGrow: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
+    flexDirection: "column",
+    justifyContent: "center"
   }
-
 });
 
-const mapStateToProps = ({ auth },ownProps ) => {
-  
-  const { navigation } =ownProps;
+const mapStateToProps = ({ auth }, ownProps) => {
+  const { navigation } = ownProps;
 
-  const { email ,password ,error, loading } = auth;
- 
+  const { email, password, error, loading } = auth;
+
   return {
-    email, password ,error ,loading,navigation
-  }
-}
+    email,
+    password,
+    error,
+    loading,
+    navigation
+  };
+};
 
-export default connect(mapStateToProps, 
-  { onEmailChanged,
-    onPasswordChanged,
-    onLoginUser,setNavigationProps
-   })(Login)
+export default connect(mapStateToProps, {
+  onEmailChanged,
+  onPasswordChanged,
+  onLoginUser,
+  setNavigationProps
+})(Login);
--- src/components/Trip/TripStory.js
+++ src/components/Trip/TripStory.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 export default class TripStory extends React.Component {
+
+    static navigationOptions = {
+        title: 'Trip Story',
+        headerTintColor: '#F1F1F2',
+        headerTitleStyle: { color: '#F1F1F2' }
+
+    };
+
     render() {
         return (
             <View>
